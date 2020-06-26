cf1294f86007091b21bcd2f3e2862fb0a1cdeaa8
Theresa29 <60735012+Theresa29@users.noreply.github.com>
2020-06-26 16:57:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody!!!!!!!!!");
+  res.send("Congrats- you built your first S2I App!");
 });
  
 module.exports.app = app;
